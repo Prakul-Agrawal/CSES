2bc959e168e7d8f200ab956dcc48612a480ebee4
Prakul-Agrawal <prakulagrawal.2003@gmail.com>
2023-01-12 19:31:53 +0530

    Final submission

---
+++ Introductory Problems/Number Spiral.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+typedef long long ll;
+typedef long double ldb;
+//#define int ll
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<vii> vvii;
+#define ff first
+#define ss second
+#define pb push_back
+#define endl '\n'
+#define all(s) s.begin(), s.end()
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define sz(x) (int)(x).size()
+#define tc int t; cin>>t; while(t--)
+#define max_heap(x) priority_queue<x>
+#define min_heap(x) priority_queue<x, vector<x>, greater<x>>
+
+void solve(){
+    ll y,x; cin >> y >> x;
+    ll location = max(x,y);
+    ll val = (location - 1) * (location - 1);
+
+    if (location % 2)
+    {
+        if (y == location) val += x;
+        else val += 2 * location - y; 
+    }
+    else
+    {
+        if (x == location) val += y;
+        else val += 2 * location - x;
+    }
+    cout << val << endl;
+}
+
+int main() 
+{
+    ios::sync_with_stdio(0); // To increase input-output speed
+    cin.tie(0);
+    tc
+    solve();
+}
