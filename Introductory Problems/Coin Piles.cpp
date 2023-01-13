3613264ddd92b93bc282712a771e3c6073c6c04f
Prakul-Agrawal <prakulagrawal.2003@gmail.com>
2023-01-13 15:34:38 +0530

    Final submission

---
+++ Introductory Problems/Coin Piles.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+typedef long long ll;
+typedef long double ldb;
+//#define int ll
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<vii> vvii;
+#define ff first
+#define ss second
+#define pb push_back
+#define endl '\n'
+#define all(s) s.begin(), s.end()
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define sz(x) (int)(x).size()
+#define tc int t; cin>>t; while(t--)
+#define max_heap(x) priority_queue<x>
+#define min_heap(x) priority_queue<x, vector<x>, greater<x>>
+
+void solve(){
+    int a,b; cin >> a >> b;
+    int small = min(a,b), diff = abs(a-b);
+    if (diff <= small && (small - diff) % 3 == 0) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int main() 
+{
+    ios::sync_with_stdio(0); // To increase input-output speed
+    cin.tie(0);
+    tc
+    solve();
+}
