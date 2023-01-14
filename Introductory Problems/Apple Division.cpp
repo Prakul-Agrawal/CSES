33cb7cc1bd4e67e9d56fa020f86a4fbd2ed9cc07
Prakul-Agrawal <prakulagrawal.2003@gmail.com>
2023-01-15 02:54:20 +0530

    Forming recursion

---
+++ Introductory Problems/Apple Division.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+typedef long long ll;
+typedef long double ldb;
+//#define int ll
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<vii> vvii;
+#define ff first
+#define ss second
+#define pb push_back
+#define endl '\n'
+#define all(s) s.begin(), s.end()
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define sz(x) (int)(x).size()
+#define tc int t; cin>>t; while(t--)
+#define max_heap(x) priority_queue<x>
+#define min_heap(x) priority_queue<x, vector<x>, greater<x>>
+
+void min_diff(vll &p, int &n, ll &total, int pos, int sum, ll &diff)
+{
+    if (pos == n)
+    {
+        diff = min(abs(total - 2 * sum), diff);
+        return;
+    }
+}
+
+void solve(){
+    int n; cin >> n;
+    vll p(n);
+    rep(i,0,n) cin >> p[i];
+    ll total = accumulate(all(p),0), val = total / 2;
+
+}
+
+int main() 
+{
+    ios::sync_with_stdio(0); // To increase input-output speed
+    cin.tie(0);
+    // tc
+    solve();
+}
