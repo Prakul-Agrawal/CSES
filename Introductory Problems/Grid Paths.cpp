ae393e84f614e00cffe223428cb790bacf08d4ec
Prakul-Agrawal <prakulagrawal.2003@gmail.com>
2023-01-17 16:38:32 +0530

    Final submission

--- Introductory Problems/Grid Paths.cpp
+++ Introductory Problems/Grid Paths.cpp
@@ -23,16 +23,58 @@ typedef vector<vii> vvii;
 #define min_heap(x) priority_queue<x, vector<x>, greater<x>>
 
 vvi grid(7, vi(7));
+int cnt = 0;
+string s;
 
-void dfs()
+bool valid(int x, int y)
+{
+    if (x == -1 || x == 7 || y == -1 || y == 7 || grid[x][y]) return false;
+    return true;
+}
+
+void dfs(int x, int y, int move)
 {
     // prune if bottom left reached before all moves complete
     // prune if cannot go forward but can go both left and right
+
+    if (x == 6 && y == 0)
+    {
+        if (move == 48) cnt++;
+        return;        
+    }
+
+    if (move == 48) return;
+
+    grid[x][y] = 1;
+
+    if (s[move] == '?' || s[move] == 'R')
+    {
+        if (valid(x,y+1) && (valid(x,y+2) || !valid(x+1,y+1) || !valid(x-1,y+1)))
+            dfs(x,y+1,move+1);
+    }
+    if (s[move] == '?' || s[move] == 'L')
+    {
+        if (valid(x,y-1) && (valid(x,y-2) || !valid(x+1,y-1) || !valid(x-1,y-1)))
+            dfs(x,y-1,move+1);
+    }
+    if (s[move] == '?' || s[move] == 'U')
+    {
+        if (valid(x-1,y) && (valid(x-2,y) || !valid(x-1,y+1) || !valid(x-1,y-1)))
+            dfs(x-1,y,move+1);
+    }
+    if (s[move] == '?' || s[move] == 'D')
+    {
+        if (valid(x+1,y) && (valid(x+2,y) || !valid(x+1,y+1) || !valid(x+1,y-1)))
+            dfs(x+1,y,move+1);
+    }
+
+    grid[x][y] = 0;
 }
 
 void solve(){
-    string s; cin >> s;
-
+    cin >> s;
+    dfs(0,0,0);
+    cout << cnt << endl;
 }
 
 int main() 
