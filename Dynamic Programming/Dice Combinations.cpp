75a2dc0a4ff0478eae272858f482bedc642b2c3a
Prakul-Agrawal <prakulagrawal.2003@gmail.com>
2023-03-17 18:07:14 +0530

    Final Submission

---
+++ Dynamic Programming/Dice Combinations.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+typedef long long ll;
+typedef long double ldb;
+//#define int ll
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<vii> vvii;
+#define ff first
+#define ss second
+#define pb push_back
+#define endl '\n'
+#define all(s) s.begin(), s.end()
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define sz(x) (int)(x).size()
+#define tc int t; cin>>t; while(t--)
+#define max_heap(x) priority_queue<x>
+#define min_heap(x) priority_queue<x, vector<x>, greater<x>>
+
+void solve(){
+    int n;
+    cin >> n;
+
+    vi dp(n+1);
+    dp[0] = 1;
+    rep(i,1,n+1)
+        rep(j,1,7)
+            if (i - j >= 0) {
+                dp[i] += dp[i-j];
+                dp[i] %= MOD;
+            }
+    cout << dp[n] << endl;
+}
+
+int main() 
+{
+    ios::sync_with_stdio(0); // To increase input-output speed
+    cin.tie(0);
+    // tc
+    solve();
+}
