7e6954f77cdf1b12d5d8116186a238da39050a01
Prakul-Agrawal <prakulagrawal.2003@gmail.com>
2023-03-17 17:53:24 +0530

    Final submission

---
+++ Dynamic Programming/Minimizing Coins.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+typedef long long ll;
+typedef long double ldb;
+//#define int ll
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<vii> vvii;
+#define ff first
+#define ss second
+#define pb push_back
+#define endl '\n'
+#define all(s) s.begin(), s.end()
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define sz(x) (int)(x).size()
+#define tc int t; cin>>t; while(t--)
+#define max_heap(x) priority_queue<x>
+#define min_heap(x) priority_queue<x, vector<x>, greater<x>>
+
+void solve(){
+    int n,x,temp;
+    cin >> n >> x;
+    vi c(n);
+    rep(i,0,n) cin >> c[i];
+
+    vi dp(x+1, -1);
+
+    dp[0] = 0;
+
+    rep(i,1,x+1){
+        temp = MOD;
+        rep(j,0,n) {
+            if (i - c[j] >= 0 && dp[i - c[j]] != -1)
+                temp = min(temp, dp[i-c[j]]+1);
+        }
+        dp[i] = temp == MOD ? -1 : temp;
+    }
+
+    cout << dp[x] << endl;
+}
+
+int main() 
+{
+    ios::sync_with_stdio(0); // To increase input-output speed
+    cin.tie(0);
+    // tc
+    solve();
+}
